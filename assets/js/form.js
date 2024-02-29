1d87f3d78d75e8d774162f1e389d284ecd3d3d29
briancordova@yahoo.com <briancordova@yahoo.com>
2024-02-28 20:51:03 -0800

    linked js file to landing page and moved functionality of the submit button to the appropriate js folder

---
+++ assets/js/form.js
@@ -0,0 +1,6 @@
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    window.location.href = 'blog.html'; // Redirect location to the blog.html page
+    });
+    
